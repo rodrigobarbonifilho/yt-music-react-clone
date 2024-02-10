e36aed99a4b8eb344aa7085c8c4270b7f903df12
Rodrigo B. <68304421+rodrigobarbonifilho@users.noreply.github.com>
2024-02-09 22:07:30 -0300

    Setting the project model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 export default function App() {
   return (
     <>
-      Hello World!
+        <Header />
+        <main>
+            <section>
+                Sidebar
+            </section>
+            <Outlet />
+        </main>
+        <Footer />
     </>
   )
 }
---
+++ src/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalCSS = createGlobalStyle`
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    text-decoration: none;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+`
